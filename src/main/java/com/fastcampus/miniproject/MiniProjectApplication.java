ea31ffb0f7c3c417cf1ead3aff07203c94087ef0
minoflower31@naver.com <minoflower31@naver.com>
2022-08-18 17:23:28 +0900

    feat: Add to projects.

---
+++ src/main/java/com/fastcampus/miniproject/MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+	}
+
+}
