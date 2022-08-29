2150594325176cf2828ab7ae8c0c2290c7199a3e
minoflower31@naver.com <minoflower31@naver.com>
2022-08-29 23:12:58 +0900

    feat: #4 MemberController 추가 및 api 구현
    세부정보 입력, 세부정보 입력할 때 건너띔, 회원 정보 상세 밎 수정이 구현된 컨트롤러

---
+++ src/main/java/com/fastcampus/miniproject/controller/MemberController.java
@@ -0,0 +1,40 @@
+package com.fastcampus.miniproject.controller;
+
+import com.fastcampus.miniproject.dto.request.MemberDetailRequest;
+import com.fastcampus.miniproject.dto.response.GetMemberResponse;
+import com.fastcampus.miniproject.dto.ResponseWrapper;
+import com.fastcampus.miniproject.dto.request.UpdateMemberRequest;
+import com.fastcampus.miniproject.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    private static Long MEMBER_ID = 1L;
+
+    @PostMapping("/join/detail")
+    public ResponseWrapper<Void> inputDetails(@RequestBody MemberDetailRequest request) {
+        memberService.inputDetails(MEMBER_ID, request);
+        return new ResponseWrapper<Void>().ok();
+    }
+
+    @GetMapping("/join/detail-skip")
+    public ResponseWrapper<Void> inputDetailsSkip() {
+        return new ResponseWrapper<Void>().noContent();
+    }
+
+    @GetMapping("/members")
+    public ResponseWrapper<GetMemberResponse> getMember() {
+        return new ResponseWrapper<>(memberService.getMember(MEMBER_ID)).ok();
+    }
+
+    @PatchMapping("/members")
+    public ResponseWrapper<Void> updatePasswordAdditionalInfo(@RequestBody UpdateMemberRequest request) {
+        memberService.updatePasswordAdditionalInfo(MEMBER_ID, request);
+        return new ResponseWrapper<Void>().ok();
+    }
+}
