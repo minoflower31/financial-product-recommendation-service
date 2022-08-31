2408d3932e272a7851dde4ac09438d4edb1701d2
minoflower31@naver.com <minoflower31@naver.com>
2022-08-31 15:36:57 +0900

    feat: 컨트롤러의 매개변수에 @AuthenticationPrincipal 추가

--- src/main/java/com/fastcampus/miniproject/controller/MemberController.java
+++ src/main/java/com/fastcampus/miniproject/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.fastcampus.miniproject.controller;
 
+import com.fastcampus.miniproject.config.auth.PrincipalDetails;
 import com.fastcampus.miniproject.dto.ResponseWrapper;
 import com.fastcampus.miniproject.dto.request.JoinMemberRequest;
 import com.fastcampus.miniproject.dto.request.MemberDetailRequest;
@@ -8,6 +9,7 @@ import com.fastcampus.miniproject.dto.response.GetMemberResponse;
 import com.fastcampus.miniproject.service.MemberService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,8 +17,6 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
-
-    private static Long MEMBER_ID = 1L;
 
     @PostMapping("/join")
     public ResponseWrapper<Void> join(@RequestBody JoinMemberRequest joinMemberRequest) {
@@ -27,8 +27,8 @@ public class MemberController {
 
     @PostMapping("/join/detail")
     //@ApiOperation(value = "세부정보 입력", notes = "회원이 세부정보를 입력한다.")
-   public ResponseWrapper<Void> inputDetails(@RequestBody MemberDetailRequest request) {
-        memberService.inputDetails(MEMBER_ID, request);
+   public ResponseWrapper<Void> inputDetails(@RequestBody MemberDetailRequest request, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        memberService.inputDetails(principalDetails.getMember().getId(), request);
         return new ResponseWrapper<Void>().ok();
     }
 
@@ -40,15 +40,15 @@ public class MemberController {
 
     @GetMapping("/members")
     //@ApiOperation(value = "회원 정보 상세", notes = "회원의 이름과 세부정보를 입력한 데이터가 조회된다.")
-    public ResponseWrapper<GetMemberResponse> getMember() {
-        return new ResponseWrapper<>(memberService.getMember(MEMBER_ID)).ok();
+    public ResponseWrapper<GetMemberResponse> getMember(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        return new ResponseWrapper<>(memberService.getMember(principalDetails.getMember().getId())).ok();
     }
 
     @PatchMapping("/members")
     //@ApiOperation(value = "회원 정보 수정", notes = "회원의 패스워드와 세부정보를 수정할 수 있다.")
-    public ResponseWrapper<Void> updatePasswordAdditionalInfo(@RequestBody UpdateMemberRequest request) {
-        memberService.updatePassword(MEMBER_ID, request);
-        memberService.updateAdditionalInfo(MEMBER_ID, request);
+    public ResponseWrapper<Void> updatePasswordAdditionalInfo(@RequestBody UpdateMemberRequest request,@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        memberService.updatePassword(principalDetails.getMember().getId(), request);
+        memberService.updateAdditionalInfo(principalDetails.getMember().getId(), request);
         return new ResponseWrapper<Void>().ok();
     }
 }
