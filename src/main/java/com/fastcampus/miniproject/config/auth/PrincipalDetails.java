4e157d3dca28018c5c0f412e2005ec42691545ea
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-31 14:17:22 +0900

    feat: leejk0924#1 로그인 및 회원가입 기능

---
+++ src/main/java/com/fastcampus/miniproject/config/auth/PrincipalDetails.java
@@ -0,0 +1,64 @@
+package com.fastcampus.miniproject.config.auth;
+
+
+import com.fastcampus.miniproject.entity.Member;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+public class PrincipalDetails implements UserDetails {
+
+    private Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> roleList = new ArrayList<>();
+        roleList.add(new GrantedAuthority() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public String getAuthority() {
+                return "ROLE_" + member.getRole();
+            }
+        });
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/fastcampus/miniproject/service/MemberService.java
+++ src/main/java/com/fastcampus/miniproject/service/MemberService.java
@@ -1,15 +1,19 @@
 package com.fastcampus.miniproject.service;
 
+import com.fastcampus.miniproject.common.JoinException;
+import com.fastcampus.miniproject.dto.request.JoinMemberRequest;
 import com.fastcampus.miniproject.dto.request.MemberDetailRequest;
 import com.fastcampus.miniproject.dto.response.GetMemberResponse;
 import com.fastcampus.miniproject.dto.request.UpdateMemberRequest;
 import com.fastcampus.miniproject.entity.Member;
 import com.fastcampus.miniproject.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,6 +21,7 @@ import java.util.NoSuchElementException;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public void inputDetails(Long memberId, MemberDetailRequest request) {
@@ -45,6 +50,17 @@ public class MemberService {
                 request.getAge());
     }
 
+    @Transactional
+    public void joinMember(JoinMemberRequest joinMemberRequest) {
+        Optional<Member> targetMember = memberRepository.findMemberByLoginId(joinMemberRequest.getEmail());
+        if (targetMember.isPresent()) {
+            throw new JoinException("이미 가입되어 있는 유저입니다.");
+        }
+        String encodingPassword = encodingPassword(joinMemberRequest.getPassword());
+        joinMemberRequest.setPassword(encodingPassword);
+        memberRepository.save(joinMemberRequest.toMember());
+    }
+
     public GetMemberResponse getMember(Long memberId) {
         Member member = findById(memberId);
         return new GetMemberResponse(member.getName(), member.getAdditionalInfo());
@@ -53,4 +69,11 @@ public class MemberService {
     Member findById(Long memberId) {
         return memberRepository.findById(memberId).orElseThrow(NoSuchElementException::new);
     }
+
+    private String encodingPassword(String password) {
+
+        String encodingPassword = bCryptPasswordEncoder.encode(password);
+        return encodingPassword;
+    }
+
 }
