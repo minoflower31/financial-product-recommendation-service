a913bb5566fde89c175cdf5a43d8d22bb319c27b
jungsunghun <jsh951227@gmail.com>
2022-09-02 23:22:26 +0900

    feat: 검색 기능, 맞춤형 상품, 현명한 소비 기능 추가

--- src/main/java/com/fastcampus/miniproject/repository/ProductRepositoryCustom.java
+++ src/main/java/com/fastcampus/miniproject/repository/ProductRepositoryCustom.java
@@ -1,12 +1,16 @@
 package com.fastcampus.miniproject.repository;
 
 import com.fastcampus.miniproject.dto.request.ProductSearchRequest;
+import com.fastcampus.miniproject.entity.AdditionalInfo;
 import com.fastcampus.miniproject.entity.Product;
+import com.fastcampus.miniproject.enums.Tag;
 
 import java.util.List;
 
 public interface ProductRepositoryCustom {
 
     List<Product> findBySearchCond(ProductSearchRequest productSearchRequest);
+    List<Product> findCustomizedList(Tag tag, AdditionalInfo additionalInfo);
+    List<Product> findWisdomList(Tag tag, AdditionalInfo additionalInfo);
 
 }
