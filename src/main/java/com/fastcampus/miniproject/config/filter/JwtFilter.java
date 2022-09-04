3514631316e9ebb6d3a5b06f2d4dd88603e28d27
Jung-Kwon Lee <akdirdl@gmail.com>
2022-09-04 22:38:21 +0900

    feat: jwt 토큰 확인

--- src/main/java/com/fastcampus/miniproject/config/filter/JwtFilter.java
+++ src/main/java/com/fastcampus/miniproject/config/filter/JwtFilter.java
@@ -25,7 +25,7 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 1. Request Header 에서 토큰을 꺼냄
         String jwt = resolveToken(request);
-
+        System.out.println(jwt);
         // 2. validateToken 으로 토큰 유효성 검사
         // 정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
